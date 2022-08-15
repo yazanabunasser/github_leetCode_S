4e7e22467db72c9ab35645fb2b0d854a95d676ab
yazanabunasser <yazanabunasser@gmail.com>
2022-08-15 13:21:24 +0300

    interfacePolygon

--- src/task1.java
+++ src/task1.java
@@ -3,8 +3,7 @@ import java.util.concurrent.atomic.AtomicIntegerArray;
 
 public class task1 {
     public static void main(String arg[]){
-        Pig pig = new Pig();
-        pig.animalSound();
-        pig.sleep();
+        Rectangle r1 = new Rectangle();
+        r1.getArea(5,6);
     }
 }
